5d4c3e96f1c7d35a889be1f23380dc0d74fcb0a2
Misaka <misaka1033@gmail.com>
2022-09-07 10:42:55 +0800

    On branch master Your branch is up to date with 'origin/master'.
    Changes to be committed:
    	new file:   doc_to_pdf/base_doc_to_pdf/__init__.py
    	new file:   doc_to_pdf/base_doc_to_pdf/base_tools.py
    	new file:   doc_to_pdf/vue_doc/vue_doc_to_pdf.py

---
+++ doc_to_pdf/base_doc_to_pdf/base_tools.py
@@ -0,0 +1,7 @@
+class BaseDocToPdf(object):
+    def __init__(self,**kv):
+        print(' BaseDocToPdf init')
+        self.menu_url = kv['menu_url']
+
+def hello():
+    pass
---
+++ doc_to_pdf/vue_doc/vue_doc_to_pdf.py
@@ -0,0 +1,9 @@
+import sys
+import os
+# sys.path.append(os.path.join('.','../base_doc_to_pdf.py') )
+sys.path.append(r'D:\workspace\libiary\my\misaka_python_practice\doc_to_pdf\base_doc_to_pdf')
+dir_list = dir()
+import base_doc_to_pdf.base_tools
+# from base_doc_to_pdf import BaseDocToPdf
+# BaseDocToPdf()
+# BaseDocToPdf()
